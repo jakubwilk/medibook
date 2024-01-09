da7e03eb9586a18cc81572121eb6e02028e94755
jakubwilk <jw@jakubwilk.pl>
2024-01-09 18:22:12 +0100

    Initialize react-query provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { FontSizeProvider } from './libs/common'
 import { UserContextProvider } from './libs/user'
 import AppContainer from './AppContainer'
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <UserContextProvider>
-      <FontSizeProvider>
-        <AppContainer />
-      </FontSizeProvider>
-    </UserContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserContextProvider>
+        <FontSizeProvider>
+          <AppContainer />
+        </FontSizeProvider>
+      </UserContextProvider>
+    </QueryClientProvider>
   )
 }
 
